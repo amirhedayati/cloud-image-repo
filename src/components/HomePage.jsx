14f0b99df520255832eb146358a4d884f193fc49
Amir Hedayati <a3hedaya@uwaterloo.ca>
2021-05-08 05:21:01 -0400

    add delete functionality

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from "react-router-dom";
 import { Image } from 'cloudinary-react';
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from 'antd';
+import './HomePage.style.css'
 
 const HomePage = () => {
     const [imageIds, setImageIds] = useState();
     const [modalImage, setModalImage] = useState();
     const [showModal, setShowModal] = useState(false);
+    const [deleted, setDeleted] = useState(false);
     const { user } = useAuth0();
+    const history = useHistory();
+
 
     useEffect(() => {
         const loadImages = async () => {
@@ -19,25 +25,49 @@ const HomePage = () => {
             }
         };
         loadImages();
-    }, [user.name]);
+        if (deleted){
+          setShowModal(false);
+        }
+        setDeleted(false);
+    }, [user.name, deleted]);
 
     const handleImageClick = ({imageId: imageId, index: index}) => {
         setShowModal(!showModal);
         setModalImage({imageId: imageId, index: index});
     }
 
+    const handleDelete = async (imageId) => {
+      try {
+              await fetch('/api/delete', {method: 'POST', headers: {"imageId": `${imageId}`}});
+              setDeleted(true);
+              const path = "\\";
+              history.push(path);
+
+
+            } catch (err) {
+                console.error(err);
+            }
+    }
+
     const Modal = () =>{
         return (
             <div className="modal">
+              <div className="row">
                 <span className="close" onClick={() => setShowModal(!showModal)}>&times;</span>
                 <Image
-                            className="modal-content"
-                            key={modalImage.index}
-                            cloudName={process.env.REACT_APP_CLOUDINARY_NAME}
-                            publicId={modalImage.imageId}
-                            crop="scale"
-                        />
-                <div className="caption"></div>
+                    className="modal-content"
+                    key={modalImage.index}
+                    cloudName={process.env.REACT_APP_CLOUDINARY_NAME}
+                    publicId={modalImage.imageId}
+                    crop="scale"
+                />
+                </div>
+                <div className="row delete-btn">
+                  <Button type="danger mt-1 delete-btn" size="large" onClick={() => handleDelete(modalImage.imageId)}>
+                    <b>Delete</b>
+                  </Button>
+                </div>
+                
             </div>
         );
     }
